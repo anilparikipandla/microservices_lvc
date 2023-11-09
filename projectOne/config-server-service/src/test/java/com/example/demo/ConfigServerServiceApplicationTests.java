81a9f899efa4b8d55a8b104d77eee0e2d2f7f4e5
anilparikipandla <anilparikipandla@gmail.com>
2023-11-09 10:11:03 +0530

    Day3: Circuit Breaker Pattern - Hystrix

---
+++ projectOne/config-server-service/src/test/java/com/example/demo/ConfigServerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
