a8dc93d86da4d5a9fc5de335953e6c16e62d70a6
anilparikipandla <anilparikipandla@gmail.com>
2023-11-10 16:48:54 +0530

    Day5: security inMemoryAuthentication

---
+++ projectOne/security-inmemory-auth-service/src/main/java/com/example/demo/HomeResource.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+
+	public HomeResource() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@GetMapping("/")
+	public String home() {
+		return ("<h1> Welcome to Security Demo</h1>");
+	}
+	
+	
+	@GetMapping("/user")
+	public String user() {
+		return ("<h1> Welcome to Security Demo - User!</h1>");
+	}
+	
+	@GetMapping("/admin")
+	public String admin() {
+		return ("<h1> Welcome to Security Demo - Admin!</h1>");
+	}
+	
+
+}
---
+++ projectOne/security-inmemory-auth-service/src/main/java/com/example/demo/SecurityConfiguration.java
@@ -0,0 +1,49 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+	public SecurityConfiguration() {
+		
+	}
+
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+		.withUser("foo")
+		.password("foo")
+		.roles("USER")
+		.and()
+		.withUser("blah")
+		.password("blah")
+		.roles("ADMIN");
+	}
+	
+	
+	
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http.authorizeRequests()
+		.antMatchers("/admin").hasRole("ADMIN")
+		.antMatchers("/user").hasAnyRole("ADMIN","USER")
+		.antMatchers("/").permitAll()
+		.and().formLogin();
+	}
+}
